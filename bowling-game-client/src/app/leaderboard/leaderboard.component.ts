ea2aa68dcb0e2cb60440eba971e5d6a14f26d9ef
TamiTop <tamiavshalom@gmail.com>
2023-04-01 21:22:38 +0300

    leaderboard table

--- bowling-game-client/src/app/leaderboard/leaderboard.component.ts
+++ bowling-game-client/src/app/leaderboard/leaderboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import axios from 'axios';
 
 @Component({
     selector: 'app-leaderboard',
@@ -7,18 +8,34 @@ import { Component } from '@angular/core';
 })
 
 export class LeaderboardComponent {
-    // temporaty
-    players: IPlayer[] = [{ name: 'Player 1', score: 200, rolls: [[1, 4], [5, 3], [6, 1], [3, 4], [5, 2], [2, 2], [8, 1], [6, 2], [7, 1], [4, 5, 3]] },
-    { name: 'Player 2', score: 180, rolls: [[4, 5], [6, 3], [1, 1], [9, 0], [8, 2], [6, 2], [7, 1], [5, 4], [9, 0], [6, 3, 2]] },
-    { name: 'Player 3', score: 210, rolls: [[3, 6], [4, 3], [7, 0], [8, 1], [5, 3], [2, 2], [9, 0], [5, 3], [7, 2], [5, 3, 2]] },
+    // Api URL
+    private readonly getLeaderboardApiUrl = "/getLeaderboard";
+
+    // Playing
+    players: IPlayer[] = [{ name: 'Player 1', scores: [10, 22, 33, 44, 57, 61, 77, 88, 99, 120], rolls: [[1, 4], [5, 3], [6, 1], [3, 4], [5, 2], [2, 2], [8, 1], [6, 2], [7, 1], [4, 5]] },
+    { name: 'Player 2', scores: [10, 22, 33, 44, 57, 61, 77, 88, 99, 120], rolls: [[4, 5], [6, 3], [1, 1], [9, 0], [8, 2], [6, 2], [7, 1], [5, 4], [9, 0], [6, 3]] },
+    { name: 'Player 3', scores: [10, 22, 33, 44, 57, 61, 77, 88, 99, 120], rolls: [[3, 6], [4, 3], [7, 0], [8, 1], [5, 3], [2, 2], [9, 0], [5, 3], [7, 2], [5, 3]] },
+    { name: 'Player 3', scores: [10, 22, 33, 44, 57, 61, 77, 88, 99, 120], rolls: [[3, 6], [4, 3], [7, 0], [8, 1], [5, 3], [2, 2], [9, 0], [5, 3], [7, 2], [6, 3, 2]] }
     ];
 
-    ngOnInit() {
-        // Sort players array in descending order by score and calculate rank of each player based on their score
-        this.players.sort((a, b) => b.score - a.score);
-        this.players.forEach((player, index) => {
-            player.place = index + 1;
-        });
+    //players: IPlayer[];
+
+    // getter setter? instead of public
+    public async getLeaderboard(): Promise<void> {
+        try {
+            // fix score to scores ffs
+            const response = await axios.get(this.getLeaderboardApiUrl);
+            if (response.data) {
+                this.players = response.data;
+                this.players.sort((a, b) => b.scores[b.scores.length - 1] - a.scores[a.scores.length - 1]);
+                this.players.forEach((player, index) => {
+                    player.place = index + 1;
+                });
+            }
+            console.log(response.data);
+        } catch (error) {
+            console.error(error);
+        }
     }
 }
 
@@ -26,5 +43,5 @@ interface IPlayer {
     place?: number,
     name: string,
     rolls: number[][],
-    score: number
+    scores: number[]
 }

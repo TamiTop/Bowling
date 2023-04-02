6eb9dd181d490fbc56293196a7200199109083d8
TamiTop <tamiavshalom@gmail.com>
2023-04-02 12:23:59 +0300

    error handling

--- bowling-game-server/controllers/bowlingController.js
+++ bowling-game-server/controllers/bowlingController.js
@@ -5,6 +5,7 @@ class BowlingController {
         this.game = new Game();
     }
 
+    // Singleton 
     static getInstance() {
         if (!BowlingController.instance) {
             BowlingController.instance = new BowlingController();
@@ -14,31 +15,37 @@ class BowlingController {
     }
 
     addRollScore(req, res) {
-        const score = req.query.score;
+        const rollScore = req.query.roll;
         const name = req.query.name;
 
-        if (!name || !score || isNaN(score) || +score > 10) {
-            res.sendStatus(400);
-        } else {
-            const player = this.game.getPlayerData(name);
-            if (player.frameIndex !== -1) {
-                player.calculateScore(+score);
-                console.table(player);
-                res.sendStatus(200);
-            } else {
-                res.status(400).send(`${name} is done playing, you can reset the game to start over.`);
+        try {
+            if (!name || !rollScore || isNaN(rollScore) || +rollScore > 10) {
+                throw new Error('Bad request');
             }
+
+            if (this.game.isGameEnded(name)) {
+                throw new Error('Game over');
+            }
+
+            this.game.addRollScore(name, rollScore);
+            res.sendStatus(200);
+        } catch (err) {
+            res.status(400).send(err);
         }
     }
 
     getLeaderboard(req, res) {
-        const leaderboard = this.game.getLeaderBoard();
-        res.status(200).send(leaderboard);
+        try {
+            const leaderboard = this.game.getLeaderBoard();
+            res.status(200).send(leaderboard);
+        } catch (err) {
+            res.status(500).send(err);
+        }
     }
 
     resetGame(req, res) {
         this.game.resetGame();
-        res.status(200).send('Game restarted.');
+        res.status(200).send('Game restarted');
     }
 }
 

70ec4d3a3ba9d23a8424a3c001e94bc1bd898351
TamiTop <tamiavshalom@gmail.com>
2023-04-02 12:24:58 +0300

    adjusted to leadeboard response structure

--- bowling-game-client/src/app/leaderboard/leaderboard.component.ts
+++ bowling-game-client/src/app/leaderboard/leaderboard.component.ts
@@ -8,29 +8,16 @@ import axios from 'axios';
 })
 
 export class LeaderboardComponent {
-    // Api URL
     private readonly getLeaderboardApiUrl = "/getLeaderboard";
+    public leaderboard: ILeaderboard;
 
-    // Playing
-    players: IPlayer[] = [{ name: 'Player 1', scores: [10, 22, 33, 44, 57, 61, 77, 88, 99, 120], rolls: [[1, 4], [5, 3], [6, 1], [3, 4], [5, 2], [2, 2], [8, 1], [6, 2], [7, 1], [4, 5]] },
-    { name: 'Player 2', scores: [10, 22, 33, 44, 57, 61, 77, 88, 99, 120], rolls: [[4, 5], [6, 3], [1, 1], [9, 0], [8, 2], [6, 2], [7, 1], [5, 4], [9, 0], [6, 3]] },
-    { name: 'Player 3', scores: [10, 22, 33, 44, 57, 61, 77, 88, 99, 120], rolls: [[3, 6], [4, 3], [7, 0], [8, 1], [5, 3], [2, 2], [9, 0], [5, 3], [7, 2], [5, 3]] },
-    { name: 'Player 3', scores: [10, 22, 33, 44, 57, 61, 77, 88, 99, 120], rolls: [[3, 6], [4, 3], [7, 0], [8, 1], [5, 3], [2, 2], [9, 0], [5, 3], [7, 2], [6, 3, 2]] }
-    ];
-
-    //players: IPlayer[];
-
-    // getter setter? instead of public
+    // Get the leaderboard from the server
     public async getLeaderboard(): Promise<void> {
         try {
-            // fix score to scores ffs
             const response = await axios.get(this.getLeaderboardApiUrl);
             if (response.data) {
-                this.players = response.data;
-                this.players.sort((a, b) => b.scores[b.scores.length - 1] - a.scores[a.scores.length - 1]);
-                this.players.forEach((player, index) => {
-                    player.place = index + 1;
-                });
+                this.leaderboard = response.data;
+                this.leaderboard.sort((a, b) => b.stats[b.stats.length - 1].score - 1 - a.stats[a.stats.length - 1].score - 1);
             }
             console.log(response.data);
         } catch (error) {
@@ -39,9 +26,15 @@ export class LeaderboardComponent {
     }
 }
 
-interface IPlayer {
-    place?: number,
+// Leaderboard type and interfaces
+type ILeaderboard = ILeaderboardItem[];
+
+interface ILeaderboardItem {
     name: string,
-    rolls: number[][],
-    scores: number[]
+    stats: IPlayerStats[]
+}
+
+interface IPlayerStats {
+    rolls: number[],
+    score: number
 }
